478804894131fd2a650e42491994ac4fab328e9c
Aleksandr Khorunzhev <sanjiksanjiksanjik@gmail.com>
2020-12-21 23:00:32 +0400

    in work

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,10 @@
-insert into persons (id, `name`) values (1, 'masha');
+insert into AUTHOR (`FULLNAME`) values ('Иванов Иван Иванович');
+insert into AUTHOR (`FULLNAME`) values ('Сидоров Сидоро Сидорович');
+
+insert into GENRE (`NAME`) values ('Фантастика');
+insert into GENRE (`NAME`) values ('Детектив');
+
+
+insert into BOOK (`TITLE`, FK_AUTHOR_ID, FK_GENRE_ID) values ('BOOK NAME1', 1, 1);
+insert into BOOK (`TITLE`, FK_AUTHOR_ID, FK_GENRE_ID) values ('BOOK NAME2', 2, 2);
+insert into BOOK (`TITLE`, FK_AUTHOR_ID, FK_GENRE_ID) values ('BOOK NAME3', 1, 2);
