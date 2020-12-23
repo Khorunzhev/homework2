14b8ff9f84f6b6f34cf037e519d47c1f0da74d96
Александр Хорунжев <horunzhev@tecforce.ru>
2020-12-23 14:57:39 +0400

    book dao CRUD

--- src/test/java/ru/khorunzhev/otus/homework2/dao/BookDaoJdbcTest.java
+++ src/test/java/ru/khorunzhev/otus/homework2/dao/BookDaoJdbcTest.java
@@ -5,9 +5,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.dao.EmptyResultDataAccessException;
 import ru.khorunzhev.otus.homework2.model.Book;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @JdbcTest
 @Import(BookDaoJdbc.class)
@@ -17,7 +19,7 @@ public class BookDaoJdbcTest {
     private BookDao bookDao;
 
     @Test
-    void insert() {
+    void shouldInsertBook() {
 
         Book expectedBook = Book.builder()
                 .id(10L)
@@ -33,6 +35,27 @@ public class BookDaoJdbcTest {
 
         assertThat(expectedBook)
                 .isEqualToIgnoringGivenFields(actualBook,"id");
+    }
+
+    @Test
+    void shouldGetBook() {
+        Book expectedBook = Book.builder()
+                .id(1L)
+                .title("Тест1")
+                .fk_author_id(1L)
+                .fk_genre_id(1L)
+                .build();
+
+        Book actualBook = bookDao.getById(1);
+
+        assertThat(actualBook).isEqualTo(expectedBook);
+    }
+
+    @Test
+    void shouldDeleteBook() {
+        long checkedId = 1;
+        bookDao.deleteById(checkedId);
+        assertThatThrownBy(() -> bookDao.getById(checkedId)).isInstanceOf(EmptyResultDataAccessException.class);
     }
 
 }
