eca70c711c9a6d8927a3f6f38f84405ece6451e2
Александр Хорунжев <horunzhev@tecforce.ru>
2020-12-23 14:59:29 +0400

    fix for PR

--- src/main/java/ru/khorunzhev/otus/homework2/dao/AuthorDao.java
+++
@@ -1,9 +0,0 @@
-package ru.khorunzhev.otus.homework2.dao;
-
-import ru.khorunzhev.otus.homework2.model.Author;
-import ru.khorunzhev.otus.homework2.model.Book;
-
-public interface AuthorDao {
-
-    Author getByFullName(String fullName);
-}
