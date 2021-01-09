ffd203e88fc9f7fe46c4e621ea997db048bf7d47
Aleksandr Khorunzhev <sanjiksanjiksanjik@gmail.com>
2021-01-09 22:47:51 +0400

    final version

--- src/test/resources/data-test.sql
+++ src/test/resources/data-test.sql
@@ -1,10 +1,13 @@
-insert into AUTHOR (`FULLNAME`) values ('Тест Автор1');
-insert into AUTHOR (`FULLNAME`) values ('Тест Автор2');
+insert into BOOK (`TITLE`) values ('NAME1');
+insert into BOOK (`TITLE`) values ('NAME2');
+insert into BOOK (`TITLE`) values ('NAME3');
 
-insert into GENRE (`NAME`) values ('Тест жанр1');
-insert into GENRE (`NAME`) values ('Тест жанр2');
+insert into AUTHOR (`FULLNAME`, BOOK_ID) values ('Иванов', 1);
+insert into AUTHOR (`FULLNAME`, BOOK_ID) values ('Сидоров', 2);
+insert into AUTHOR (`FULLNAME`, BOOK_ID) values ('Иванов', 3);
 
+insert into GENRE (`NAME`, BOOK_ID) values ('Фантастика', 1);
+insert into GENRE (`NAME`, BOOK_ID) values ('Детектив', 2);
+insert into GENRE (`NAME`, BOOK_ID) values ('Детектив', 3);
 
-insert into BOOK (`TITLE`, AUTHOR_ID, GENRE_ID) values ('Тест1', 1, 1);
-insert into BOOK (`TITLE`, AUTHOR_ID, GENRE_ID) values ('Тест2', 2, 2);
-insert into BOOK (`TITLE`, AUTHOR_ID, GENRE_ID) values ('Тест3', 1, 2);
+insert into COMMENT (`TEXT`, BOOK_ID) values ('Коммент', 1);
