328b58fcc86a9f030721b70f59287434cfc5cd87
Aleksandr Khorunzhev <sanjiksanjiksanjik@gmail.com>
2021-02-13 20:39:06 +0400

    init commit

--- src/main/java/ru/khorunzhev/otus/homework2/Main.java
+++ src/main/java/ru/khorunzhev/otus/homework2/Main.java
@@ -4,12 +4,11 @@ package ru.khorunzhev.otus.homework2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.sql.SQLException;
 
 @SpringBootApplication
 public class Main {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         SpringApplication.run(Main.class);
     }
 }
--- src/main/java/ru/khorunzhev/otus/homework2/service/BookServiceImpl.java
+++ src/main/java/ru/khorunzhev/otus/homework2/service/BookServiceImpl.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
 @Log
 public class BookServiceImpl implements BookService {
 
