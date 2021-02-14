3fe6b0cdf36dcc35b4ff1f910d022477ed3b204c
Aleksandr Khorunzhev <sanjiksanjiksanjik@gmail.com>
2021-02-14 20:07:31 +0400

    какая-то дичь происходит

--- src/main/java/ru/khorunzhev/otus/homework2/Main.java
+++ src/main/java/ru/khorunzhev/otus/homework2/Main.java
@@ -5,6 +5,7 @@ import com.github.cloudyrock.spring.v5.EnableMongock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
 @EnableMongock
 @EnableMongoRepositories
