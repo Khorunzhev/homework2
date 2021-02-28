7dd3354d8f53bec41b315cf2b0ade426a5590851
Aleksandr Khorunzhev <sanjiksanjiksanjik@gmail.com>
2021-02-28 15:51:13 +0400

    in process

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,3 +9,5 @@ insert into BOOK (`TITLE`, AUTHOR_ID, GENRE_ID) values ('NAME2', 2, 2);
 insert into BOOK (`TITLE`, AUTHOR_ID, GENRE_ID) values ('NAME3', 1, 1);
 
 insert into COMMENT (`TEXT`, BOOK_ID) values ('Коммент', 1);
+
+insert into USERS (`USERNAME`, `PASSWORD`) values ('admin', 'admin1');
